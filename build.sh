54a7c0396d58dc7fdc7cfca8cfa2b9e64e5107ea
David <17292606+reznik-gh@users.noreply.github.com>
2025-04-20 12:57:14 +0200

    rpmfusion

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -oue pipefail
 echo "--========== Installiere Terminal Zeug ==========--"
 /ctx/modules/terminal-packages.sh
 
+echo "--========== Installiere RPMfusion und nvidia ==========--"
+/ctx/modules/rpmfusion.sh
+
 # echo "--========== Installiere Gnome Desktop ==========--"
 # /ctx/modules/gnome.sh
---
+++ modules/rpmfusion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set ${SET_X:+-x} -oue pipefail
+
+dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install akmod-nvidia
