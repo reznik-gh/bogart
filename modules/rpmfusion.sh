308509f31abbd0eb51affb999c2b6478f5bf6086
David <17292606+reznik-gh@users.noreply.github.com>
2025-04-20 14:22:22 +0200

    Flüchtigkeitsfehler...

--- modules/rpmfusion.sh
+++ modules/rpmfusion.sh
@@ -3,6 +3,6 @@
 set ${SET_X:+-x} -oue pipefail
 
 dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install --assumeyes https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 dnf install akmod-nvidia
